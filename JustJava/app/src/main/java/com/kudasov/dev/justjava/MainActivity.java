f2706283f471492fb1b1a5bff2fbbad9549a39f2
Dmitriy Kudasov <dev.kudasov@gmail.com>
2021-08-24 15:44:36 +0300

    Add onClick method

--- JustJava/app/src/main/java/com/kudasov/dev/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/kudasov/dev/justjava/MainActivity.java
@@ -3,6 +3,8 @@ package com.kudasov.dev.justjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void submitOrder(View view) {
+        display(1);
+    }
+
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_count);
+        quantityTextView.setText("" + number);
     }
 }
