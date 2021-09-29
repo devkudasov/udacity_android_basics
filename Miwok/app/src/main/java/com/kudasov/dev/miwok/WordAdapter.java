38319c871d77e50ede755003029cd1c76fb3d9ae
Dmitriy Kudasov <dev.kudasov@gmail.com>
2021-09-30 00:34:54 +0300

    Miwok: Create Word class and WordAdapter
    Add all needed words

---
+++ Miwok/app/src/main/java/com/kudasov/dev/miwok/WordAdapter.java
@@ -0,0 +1,39 @@
+package com.kudasov.dev.miwok;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class WordAdapter extends ArrayAdapter<Word> {
+    public WordAdapter(Activity activity, ArrayList<Word> words) {
+        super(activity, 0, words);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View listItemView = convertView;
+
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.word_item, parent, false);
+        }
+
+        Word currentWord = getItem(position);
+
+        TextView miwokTextView = listItemView.findViewById(R.id.miwok_word);
+        miwokTextView.setText(currentWord.getMiwokTranslation());
+
+        TextView defaultTextView = listItemView.findViewById(R.id.english_word);
+        defaultTextView.setText(currentWord.getDefaultTranslation());
+
+        return listItemView;
+    }
+}
