c5eb6ba792cc10dee7d19e6b2100b7bb17c244cb
Dmitriy Kudasov <dev.kudasov@gmail.com>
2021-10-27 10:42:44 +0300

    MusicApp: Create activities

---
+++ MusicApp/app/src/main/java/com/kudasov/dev/musicapp/ArtistListActivity.java
@@ -0,0 +1,14 @@
+package com.kudasov.dev.musicapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ArtistListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_artist_list);
+    }
+}
